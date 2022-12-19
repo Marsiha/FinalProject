5a99dbb1333b4c86320bbe118503ef537b4f1417
Adilet <119230294+siweirdb@users.noreply.github.com>
2022-12-19 19:19:21 +0600

    Update list2js.js

--- list2js.js
+++ list2js.js
@@ -14,7 +14,10 @@ var title = document.getElementById('im');
 var opi = document.getElementById('opi');
 var cost = document.getElementById('cost');
 var arr = ["Medium rare steak roasting degree is an ideal roasting for classic steaks, red-pink color inside. The temperature is 46-50 °C.",
-    "blya","zb"];
+    "Soup without meat, but no less delicious than with it. A lot of vitamins, trace elements and few calories. It is very useful and can be recommended for baby food and for adults. A set of vegetables can be any to taste.",
+    "Pizza Margarita is a traditional Italian pizza. The composition of this pizza is extremely simple, its main ingredients: mozzarella cheese, ripe tomatoes and fresh basil leaves.",
+    "This dish is a highlight of Ukrainian cuisine, which is probably known all over the world. Rich, appetizing and satisfying ... a wonderful idea for the whole family!",
+    ""];
 Array.from(document.getElementsByClassName('card')).forEach((ele,i)=>{
     ele.addEventListener('click', ()=>{
         photo.src = ele.getElementsByTagName('img')[0].src;
